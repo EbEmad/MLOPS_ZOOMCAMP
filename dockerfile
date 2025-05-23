c388a3e6b408ad09075dbdcfde324e1078079bce
Ebrahim Emad <ebemad8@gmail.com>
2025-05-23 21:43:46 +0300

    adding mlflow structure

--- dockerfile
+++ dockerfile
@@ -5,7 +5,11 @@ WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
+
+RUN pip install --upgrade pip && \
+    pip install mlflow[extras] psycopg2-binary boto3
+
 COPY run.sh /app/run.sh
 RUN chmod +x /app/run.sh
-
+EXPOSE 5000
 CMD [ "bash", "/app/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+echo "Starting MLflow server..."
+mlflow server \
+  --backend-store-uri postgresql://mlflow:mlflow@db:5432/mlflow \
+  --default-artifact-root s3://mlflow/ \
+  --host 0.0.0.0 \
+  --port 5000 &   # run in background (&)
+
+echo "Starting Jupyter Notebook..."
 jupyter notebook \
   --ip=0.0.0.0 \
   --port=8000 \
@@ -6,3 +15,5 @@ jupyter notebook \
   --no-browser \
   --NotebookApp.token='' \
   --NotebookApp.password=''
+
+wait  # wait for all background jobs
